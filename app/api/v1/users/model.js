c3eaac029a14acd7be99dd5ab0ff800419f4adb6
Muhammad Ryan Fikri Fakhrezi <113404595+Yanfiq@users.noreply.github.com>
2025-04-14 15:05:22 +0700

    move all backend code to app dir

--- app/api/v1/users/model.js
+++ app/api/v1/users/model.js
@@ -1,16 +1,5 @@
 const mongoose = require("mongoose");
 
-/**
- * @typedef {Object} User
- * @property {string} id
- * @property {string} name
- * @property {string} email
- * @property {string} password
- * @property {string} phone
- * @property {string} domicile
- * @property {string} image
- */
-
 const UserSchema = new mongoose.Schema({
   fullName: {type: String, required: true},
   email: {type: String, required: true, unique: true},
