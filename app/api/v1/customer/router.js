7dda29c962f4346989a0a0352d7ba7ad67286471
Muhammad Ryan Fikri Fakhrezi <113404595+Yanfiq@users.noreply.github.com>
2025-04-27 03:54:55 +0700

    checkout

--- app/api/v1/customer/router.js
+++ app/api/v1/customer/router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const router = express.Router();
-const { signinController, signupController, getProfileController, editProfileController } = require("./controller");
+const { signinController, signupController, getProfileController, editProfileController, checkoutController } = require("./controller");
 const upload = require("../../../middlewares/upload");
 const { authenticateUser } = require("../../../middlewares/auth");
 
@@ -9,5 +9,7 @@ router.post("/customer/auth/signup", signupController);
 router.post("/customer/auth/signin", signinController);
 router.get("/customer/profile", authenticateUser, getProfileController)
 router.post("/customer/edit", authenticateUser, upload.single('image'), editProfileController)
+
+router.post("/vehicles/:id/checkout", authenticateUser, checkoutController)
 
 module.exports = router;
