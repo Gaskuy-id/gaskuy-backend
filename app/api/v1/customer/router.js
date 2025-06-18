ffd18db42908c3c3f39fec6f9bda7f2e37397496
Muhammad Ryan Fikri Fakhrezi <113404595+Yanfiq@users.noreply.github.com>
2025-06-18 12:17:55 +0700

    get rental history

--- app/api/v1/customer/router.js
+++ app/api/v1/customer/router.js
@@ -1,12 +1,13 @@
 const express = require("express");
 
 const router = express.Router();
-const { signinController, signupController, getProfileController, editProfileController, checkoutController, getAvailableVehiclesController } = require("./controller");
+const { signinController, signupController, getProfileController, editProfileController, checkoutController, getAvailableVehiclesController, getAllRentalHistoryController } = require("./controller");
 const upload = require("../../../middlewares/upload");
 const { authenticateUser } = require("../../../middlewares/auth");
 
 router.get("/vehicles", getAvailableVehiclesController);
 router.get("/customer/profile", authenticateUser, getProfileController)
+router.get("/customer/history", authenticateUser, getAllRentalHistoryController)
 //router.post("/customer/edit", authenticateUser, upload.single('image'), editProfileController)
 
 router.post("/vehicles/:id/checkout", authenticateUser, checkoutController)
