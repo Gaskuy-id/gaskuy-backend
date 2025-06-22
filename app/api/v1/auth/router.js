a183a0da0c7cc699761e57bb23c4217e0a92be59
Muhammad Faqih Khawarizmi <muhammadfaqihk16@gmail.com>
2025-06-23 04:18:49 +0700

    ubah password done

--- app/api/v1/auth/router.js
+++ app/api/v1/auth/router.js
@@ -1,9 +1,13 @@
 const express = require("express");
-
 const router = express.Router();
-const { signinController, signupController } = require("./controller");
+const { 
+  signinController, 
+  signupController,
+  changePasswordController 
+} = require("./controller");
 
 router.post("/auth/signup", signupController);
 router.post("/auth/signin", signinController);
+router.post("/auth/change-password", changePasswordController);
 
 module.exports = router;
